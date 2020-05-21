510784ebe37d5583d6db85ae4ac9fb31ff07d52b
cdmiddle <cdmiddle@umich.edu>
2020-05-21 18:57:56 -0400

    added functionality for partcipating in future releases and fully automated the process with the insertion of CVV code on a credit/debit card

--- HypeBot.py
+++ HypeBot.py
@@ -1,4 +1,5 @@
 import time
+import datetime
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.support.ui import WebDriverWait
@@ -6,6 +7,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import StaleElementReferenceException
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from random import randrange
 
 
 def scroll_shim(passed_in_driver, object):
@@ -18,10 +20,6 @@ def scroll_shim(passed_in_driver, object):
         scroll_nav_out_of_way = 'window.scrollBy(0, -240);'
         passed_in_driver.execute_script(scroll_by_coord)
         passed_in_driver.execute_script(scroll_nav_out_of_way)
-
-# def check_cart(passed_in_driver):
-#     # cart_count = passed_in_driver.find_element_by_xpath("//span[@class='cart-count-jewel small fs10-sm lh10-sm d-sm-b text-color-white bg-accent ta-sm-c']").getAttribute("innerHTML")
-#     return cart_count
 
 
 def clk_button(passed_in_driver, object):
@@ -44,10 +42,19 @@ def wait_for_invisibility(object):
             pass
     except StaleElementReferenceException as e:
         pass
+    time.sleep(1)
 
 def capture_page(driver, name):
     with open(name, "x") as f:
         f.write(driver.page_source)
+
+
+def find_element(xpath):
+    try:
+        button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
+        return button
+    except TimeoutError:
+        print('timedout :/')
 
 
 
@@ -57,9 +64,12 @@ driver = webdriver.Firefox()
 passW = 'Bob9100t'
 emailA = 'middletoncourt@gmail.com'
 shoe_size = '13'
+cvv = '132'
+release_date = datetime.datetime(2020, 5, 21, 17, 59)
+now = datetime.datetime.now()
 
 
-driver.get('https://www.nike.com/launch/t/acg-moc-3-0-tie-dye')
+driver.get('https://www.nike.com/launch/t/pg-4-pcg')
 
 # find the login button
 login_icon = driver.find_element_by_xpath("//button[@class='join-log-in text-color-grey prl3-sm pt2-sm pb2-sm fs12-sm d-sm-b']")
@@ -80,57 +90,51 @@ login_window = driver.find_element_by_xpath("//div[@class = 'ncss-col-sm-12 full
 
 wait_for_invisibility(login_window)
 
+# wait until the release time and then refresh the page
+# time.sleep((release_date - now).seconds)
+
+driver.refresh()
+
 
 # # pick your size and add to cart
-# # while (check_cart(driver) < 1)
 
-time.sleep(1)
+size_btn = find_element("//button[starts-with(text(), 'M " + shoe_size + "') or starts-with(text(), " + shoe_size + ")]")
+# capture_page(driver, 'clcik.html')
 
-try:
-    xpath = ("//button[starts-with(text(), 'M 11') or starts-with(text(), '11') and not(@disabled)]")
-    # size_btn = WebDriverWait(driver, 10).until(
-    #         lambda driver : driver.find_element_by_xpath(xpath))
-    size_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, "//button[starts-with(text(), 'M " + shoe_size + "') or starts-with(text(), " + shoe_size + ") and not(@disabled)]")))
-    capture_page(driver, "size.html")
-finally:
-    print('size selected')
 
 clk_button(driver, size_btn)
 
 
-addToCart_btn = driver.find_element_by_xpath("//div[@class = 'mt2-sm mb6-sm prl0-lg fs14-sm']/button[1]")
+addToCart_btn = find_element("//div[@class = 'mt2-sm mb6-sm prl0-lg fs14-sm']/button[1]")
 
-capture_page(driver, 'addtocart.html')
+# capture_page(driver, 'addtocart.html')
 
 clk_button(driver, addToCart_btn)
 
 
 driver.get('https://www.nike.com/us/en/cart')
 
-try:
-    xpath = ("//div[@class = 'ncss-col-sm-12 css-gajhq5']/button[@class = 'css-8k8rmj e1qel1sl4']")
-    # size_btn = WebDriverWait(driver, 10).until(
-    #         lambda driver : driver.find_element_by_xpath(xpath))
-    checkout_btn = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, xpath)))
-    capture_page(driver, "cart.html")
-except TimeoutError:
-    print('took too long')
+checkout_btn = find_element("//div[@class = 'ncss-col-sm-12 css-gajhq5']/button[@class = 'css-8k8rmj e1qel1sl4']")
 
-# checkout_btn = driver.find_element_by_xpath("//div[@class = 'ncss-col-sm-12 css-gajhq5']/button[@class = 'css-8k8rmj e1qel1sl4']")
 # capture_page(driver, "checkout.html")
 
 clk_button(driver, checkout_btn)
 
-# email_entry = driver.find_element_by_xpath("//input[@name = 'emailAddress']")
-# pass_entry = driver.find_element_by_xpath("//input[@name = 'password']")
-# login_button = driver.find_element_by_xpath("//input[@value = 'MEMBER CHECKOUT']")
 
-# email_entry.send_keys(emailA)
-# pass_entry.send_keys(passW)
-# clk_button(login_button)
+iframe = find_element("//iframe[@class = 'credit-card-iframe-cvv mt1 u-full-width']")
 
+driver.switch_to.frame(iframe)
+
+cvv_input = find_element("//input[@class = 'mod-input ncss-input pt2-sm pr4-sm pb2-sm pl4-sm']")
+
+cvv_input.send_keys(cvv)
+
+driver.switch_to.default_content()
 
 # placeOrder_btn = driver.find_element_by_xpath("//button[contains(text(), 'Place order')]")
+placeOrder_btn = find_element("//button[@class = 'd-lg-ib fs14-sm ncss-brand ncss-btn-accent pb2-lg pb3-sm prl5-sm pt2-lg pt3-sm u-uppercase']")
+# capture_page(driver, "order.html")
+clk_button(driver, placeOrder_btn)
 
 
 
